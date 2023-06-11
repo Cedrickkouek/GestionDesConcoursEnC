6218f41d9c8f59e8d837f5db5484b99f8b508129
cedrickkouek <cedrickkouek@gmail.com>
2023-06-11 10:45:58 +0100

    J'ai ajouter la fonction supprimer

--- main.c
+++ main.c
@@ -3,6 +3,11 @@
 #include <string.h>
 #include <stdbool.h>
 
+#define TAILLE_FICHIER 1024
+#define LINE_MAX 2048
+
+FILE *file, *temp;
+
 typedef struct {
     int ncin;
     int age;
@@ -12,24 +17,163 @@ typedef struct {
     char decison[100];
 } candidat;
 
+candidat can1;
 
+//void rechercher(int cni);
 void saisir(void);
+int checkcni(int cni);
+void supprimer(int ncni);
 
 int main()
 {
-    saisir();
+    //saisir();
+    supprimer(1234567);
+    //rechercher(122132);
     return 0;
+}
+
+int checkcni(int cni)
+{
+    FILE *P;
+    int i;
+    P = fopen("concours.txt", "a+");
+
+    do
+    {
+        fscanf(P, "%d\n%d\n%s\n%s\n", &can1.ncin, &can1.age, can1.nom, can1.prenom /*&can1.notes[i], /*&can1.decision*/);
+
+        for(i=1; i<=10; i++)
+        {
+            fscanf(P, "%f\n", &can1.notes[i]);
+        }
+        
+        fscanf(P, "%s\n", can1.decison);
+
+        fflush(stdin);
+
+        if(can1.ncin==cni)
+        {
+            fclose(P);
+            return 1;
+        }
+
+    } while (!feof(P));
+    fclose(P);
+    return -1;
+}
+
+void supprimer(int ncni)
+{
+    //char nomfichier[TAILLE_FICHIER];
+    //char nomfichier_temp[TAILLE_FICHIER];
+    //char buffer[LINE_MAX];
+
+    char rep;
+    
+    int i;
+    
+    //printf("Veuillez entrer le nom")
+
+    fflush(stdin);
+
+    if(checkcni(ncni) == 1)
+    {
+        printf("\n Voulez vous vraiment supprimer le Candidat ? (o/n)");
+        scanf("%c", &rep);
+        fflush(stdin);
+
+        if(rep == 'o' || rep == 'O')
+        {
+            FILE *Prend, *F;
+
+            Prend = fopen("concours.txt", "a+");
+            F = fopen("temp_concours.txt", "a+");
+            do
+            {
+                fscanf(Prend, "%d\n%d\n%s\n%s\n", &can1.ncin, &can1.age, can1.nom, can1.prenom /*&can1.notes[i], /*&can1.decision*/);
+
+                 for(i=1; i<=10; i++)
+                     {
+                         fscanf(Prend, "%f\n", &can1.notes[i]);
+                     }
+                fscanf(Prend, "%s\n", can1.decison);
+
+                if(ncni != can1.ncin)
+                {
+                   fprintf(F, "%d\n%d\n%s\n%s\n", ncni, can1.age, can1.nom, can1.prenom);
+
+                    for(i=0; i<10; i++)
+                    {
+                        fprintf(F, "%f\n", can1.notes[i]);
+                    }
+                    fprintf(F, "%s\n", can1.decison);
+                }
+            } while (!feof(Prend));
+            fclose(Prend);
+            fclose(F);
+            remove("concours.txt");
+            rename("temp_concours.txt", "concours.txt");
+            printf("\nSuppression effectuée avec succès\n");
+        }
+    }
+    else
+    {
+        printf("\n Cette CNI de candidat n'existe pas\n");
+    }
+
+    /*strcpy(nomfichier_temp, "temp___");
+    strcat(nomfichier_temp, "concours.txt");
+
+    file = fopen("concours.txt", "r");
+    temp = fopen(nomfichier_temp, "w");
+
+    if(file == NULL || temp == NULL)
+    {
+        printf("Impossible d'ouvrir le(s) fichier(s)");
+        return;
+    }
+
+    bool toujourslire = true;
+    int line_actu = 1;
+
+    do
+    {
+        fgets(buffer, LINE_MAX, file);
+        if(feof(file))
+        {
+            toujourslire = false;
+        }
+        else if(line_actu != cni)
+        {
+            fputs(buffer, temp);
+        }
+        //else if(line_actu == cni)
+        //{
+            //for(i=line_actu; i <= line_actu+15; i++)
+            //{
+                //continue;
+            //}
+        //}
+        line_actu++;
+
+    } while(toujourslire);
+
+    fclose(file);
+    fclose(temp);
+
+    remove("concours.txt");
+    rename(nomfichier_temp, "concours.txt");*/
+
 }
 
 void saisir(void)
 {
-    candidat can1;
     int i, newcni, trouver=0;
     char autre[100];
     bool etatnote[10];
     bool verifnote;
     float sum=0, moy=0;
-    FILE *file;
+
     file=fopen("concours.txt", "at+");
 
     if(file==NULL)
@@ -149,8 +293,8 @@ void saisir(void)
             strcpy(can1.decison, "Admis");
         }
 
-        strcpy(autre, "-----------------\n");
-        fprintf(file, "%s\n", autre);
+        //strcpy(autre, "-----------------\n");
+        //fprintf(file, "%s\n", autre);
 
         fprintf(file, "%d\n%d\n%s\n%s\n", newcni, can1.age, can1.nom, can1.prenom);
 
@@ -159,11 +303,45 @@ void saisir(void)
             fprintf(file, "%f\n", can1.notes[i]);
         }
         fprintf(file, "%s\n", can1.decison);
-        strcpy(autre, "-----------------\n");
-        fprintf(file, "%s\n", autre);
+        //strcpy(autre, "-----------------\n");
+        //fprintf(file, "%s\n", autre);
     }
     fclose(file);
 
 }
+
+/**void rechercher(int cni)
+{
+    int i;
+    //printf("Veuillez entrer le numero de CNI de l'etudiant à rechercher ?\n");
+    //scanf("%d", &supetu);
+
+    file = fopen("concours.txt", "a+");
+    do
+    {
+        //fscanf(file, "%d\n%d\n%s\n%s\n", &can1.ncin, &can1.age, can1.nom, can1.prenom /*&can1.notes[i], /*&can1.decision*/
+
+        /*for(i=1; i<=10; i++)
+        {
+            fscanf(file, "%f\n", &can1.notes[i]);
+        }
+        fscanf(file, "%s\n", can1.decison);
+
+        if(cni == can1.ncin)
+         {
+            printf("-----------Informations sur le candidat--------\n\n");
+            printf("Cni\t: %d\n", can1.ncin);
+            printf("Age\t: %d \n", can1.age);
+            printf("Nom\t: %s \n", can1.nom);
+            printf("Prenom\t: %s \n", can1.prenom);
+            for(i=0; i<10; i++)
+            {
+                printf("\n Note numero %d: %f\n", i+1, can1.notes[i]);
+            }
+            printf("Decision\t: %s\n", can1.decison);
+         }
+    } while (!feof(file));
+    fclose(file);
+}*/
 
 
